e74dba0439df4af9caea18121b5708376cd9a338
original <git@github.com:denis-poroshin/variables.git>
2024-06-10 17:41:13 +0300

    Начало

---
+++ src/main/java/pro/sky/skyprospringmokito/SkyproSpringMokitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringmokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringMokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringMokitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringmokito/SkyproSpringMokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringmokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringMokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
